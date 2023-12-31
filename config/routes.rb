9c0b6c0683ed085f20bab7964a072219ad81bc5a
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2023-12-31 06:38:40 +0000

    add(2) 応用課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
   get "home/about"=>"homes#about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
