f53e87db762e0d7260412543082fe697a08cd564
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-05 07:35:01 +0000

    応用課題4(2)

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :relationships, class_name: "relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :relationships, 
+  
+  has_many :relationships, foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :relationships, source: :follower
+  
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :reverse_of_relationships, source: :followed
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
@@ -21,5 +26,17 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
   end
 end
